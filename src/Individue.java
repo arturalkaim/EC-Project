3c03e4e014497a1ac3fe473c24156f98c5f021a2
arturalkaim <arturalkaim@gmail.com>
2014-10-01 14:32:42 +0100

    Individue class added

---
+++ src/Individue.java
@@ -0,0 +1,35 @@
+
+public class Individue {
+	double[] gen;
+	Double fitness;
+	int rank;
+	/**
+	 * @param gen
+	 * @param fitness
+	 * @param rank
+	 */
+	public Individue(double[] gen, Double fitness) {
+		super();
+		this.gen = gen;
+		this.fitness = fitness;
+	}
+	public double[] getGen() {
+		return gen;
+	}
+	public void setGen(double[] gen) {
+		this.gen = gen;
+	}
+	public Double getFitness() {
+		return fitness;
+	}
+	public void setFitness(Double fitness) {
+		this.fitness = fitness;
+	}
+	public int getRank() {
+		return rank;
+	}
+	public void setRank(int rank) {
+		this.rank = rank;
+	}
+	
+}
